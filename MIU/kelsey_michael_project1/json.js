3a94e8cee71a6bb17e5b4735310d621aeec4a6f4
Michael Kelsey <kelseym@fullsail.edu>
2012-02-01 21:21:00 -0500

    Copied activities to create 20 of them.  Not uniquely named yet

--- MIU/kelsey_michael_project1/json.js
+++ MIU/kelsey_michael_project1/json.js
@@ -27,4 +27,141 @@ var json = {
 		"date": ["Date Due:", "2012-01-27"],
 		"notes": ["Notes:", "Starts @ 1600"]		
 	}
+	"activity4": {
+		"category": ["Group:", "Errand"],
+		"name": ["Name of Activity:", "Grocery Shopping"],
+		"help": ["Needs Assistance:", "No"],
+		"time": ["Duration:", "3"],
+		"date": ["Date Due:", "2012-01-31"],
+		"notes": ["Notes:", "Pick up items for BBQ"]		
+	},
+	"activity5": {
+		"category": ["Group:", "Chore"],
+		"name": ["Name of Activity:", "Add attic access stairs"],
+		"help": ["Needs Assistance:", "Yes"],
+		"time": ["Duration:", "6"],
+		"date": ["Date Due:", "2012-01-29"],
+		"notes": ["Notes:", "Buy pull down ladder from Lowes"]		
+	},
+	"activity6": {
+		"category": ["Group:", "Me-Time"],
+		"name": ["Name of Activity:", "MTG Prerelease"],
+		"help": ["Needs Assistance:", "No"],
+		"time": ["Duration:", "4"],
+		"date": ["Date Due:", "2012-01-27"],
+		"notes": ["Notes:", "Starts @ 1600"]		
+	}
+	"activity7": {
+		"category": ["Group:", "Errand"],
+		"name": ["Name of Activity:", "Grocery Shopping"],
+		"help": ["Needs Assistance:", "No"],
+		"time": ["Duration:", "3"],
+		"date": ["Date Due:", "2012-01-31"],
+		"notes": ["Notes:", "Pick up items for BBQ"]		
+	},
+	"activity8": {
+		"category": ["Group:", "Chore"],
+		"name": ["Name of Activity:", "Add attic access stairs"],
+		"help": ["Needs Assistance:", "Yes"],
+		"time": ["Duration:", "6"],
+		"date": ["Date Due:", "2012-01-29"],
+		"notes": ["Notes:", "Buy pull down ladder from Lowes"]		
+	},
+	"activity9": {
+		"category": ["Group:", "Me-Time"],
+		"name": ["Name of Activity:", "MTG Prerelease"],
+		"help": ["Needs Assistance:", "No"],
+		"time": ["Duration:", "4"],
+		"date": ["Date Due:", "2012-01-27"],
+		"notes": ["Notes:", "Starts @ 1600"]		
+	}
+	"activity10": {
+		"category": ["Group:", "Errand"],
+		"name": ["Name of Activity:", "Grocery Shopping"],
+		"help": ["Needs Assistance:", "No"],
+		"time": ["Duration:", "3"],
+		"date": ["Date Due:", "2012-01-31"],
+		"notes": ["Notes:", "Pick up items for BBQ"]		
+	},
+	"activity11": {
+		"category": ["Group:", "Chore"],
+		"name": ["Name of Activity:", "Add attic access stairs"],
+		"help": ["Needs Assistance:", "Yes"],
+		"time": ["Duration:", "6"],
+		"date": ["Date Due:", "2012-01-29"],
+		"notes": ["Notes:", "Buy pull down ladder from Lowes"]		
+	},
+	"activity12": {
+		"category": ["Group:", "Me-Time"],
+		"name": ["Name of Activity:", "MTG Prerelease"],
+		"help": ["Needs Assistance:", "No"],
+		"time": ["Duration:", "4"],
+		"date": ["Date Due:", "2012-01-27"],
+		"notes": ["Notes:", "Starts @ 1600"]		
+	}
+	"activity13": {
+		"category": ["Group:", "Errand"],
+		"name": ["Name of Activity:", "Grocery Shopping"],
+		"help": ["Needs Assistance:", "No"],
+		"time": ["Duration:", "3"],
+		"date": ["Date Due:", "2012-01-31"],
+		"notes": ["Notes:", "Pick up items for BBQ"]		
+	},
+	"activity14": {
+		"category": ["Group:", "Chore"],
+		"name": ["Name of Activity:", "Add attic access stairs"],
+		"help": ["Needs Assistance:", "Yes"],
+		"time": ["Duration:", "6"],
+		"date": ["Date Due:", "2012-01-29"],
+		"notes": ["Notes:", "Buy pull down ladder from Lowes"]		
+	},
+	"activity15": {
+		"category": ["Group:", "Me-Time"],
+		"name": ["Name of Activity:", "MTG Prerelease"],
+		"help": ["Needs Assistance:", "No"],
+		"time": ["Duration:", "4"],
+		"date": ["Date Due:", "2012-01-27"],
+		"notes": ["Notes:", "Starts @ 1600"]		
+	}
+	"activity16": {
+		"category": ["Group:", "Errand"],
+		"name": ["Name of Activity:", "Grocery Shopping"],
+		"help": ["Needs Assistance:", "No"],
+		"time": ["Duration:", "3"],
+		"date": ["Date Due:", "2012-01-31"],
+		"notes": ["Notes:", "Pick up items for BBQ"]		
+	},
+	"activity17": {
+		"category": ["Group:", "Chore"],
+		"name": ["Name of Activity:", "Add attic access stairs"],
+		"help": ["Needs Assistance:", "Yes"],
+		"time": ["Duration:", "6"],
+		"date": ["Date Due:", "2012-01-29"],
+		"notes": ["Notes:", "Buy pull down ladder from Lowes"]		
+	},
+	"activity18": {
+		"category": ["Group:", "Me-Time"],
+		"name": ["Name of Activity:", "MTG Prerelease"],
+		"help": ["Needs Assistance:", "No"],
+		"time": ["Duration:", "4"],
+		"date": ["Date Due:", "2012-01-27"],
+		"notes": ["Notes:", "Starts @ 1600"]		
+	}
+	"activity19": {
+		"category": ["Group:", "Errand"],
+		"name": ["Name of Activity:", "Grocery Shopping"],
+		"help": ["Needs Assistance:", "No"],
+		"time": ["Duration:", "3"],
+		"date": ["Date Due:", "2012-01-31"],
+		"notes": ["Notes:", "Pick up items for BBQ"]		
+	},
+	"activity20": {
+		"category": ["Group:", "Chore"],
+		"name": ["Name of Activity:", "Add attic access stairs"],
+		"help": ["Needs Assistance:", "Yes"],
+		"time": ["Duration:", "6"],
+		"date": ["Date Due:", "2012-01-29"],
+		"notes": ["Notes:", "Buy pull down ladder from Lowes"]		
+	},
+			
 }
