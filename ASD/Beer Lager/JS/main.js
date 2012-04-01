e5e1ca191abd959cb9853ce52bc39df11f5e6e35
Michael Kelsey <kelseym@fullsail.edu>
2012-04-01 18:52:15 -0400

    Added Global Variables

--- ASD/Beer Lager/JS/main.js
+++ ASD/Beer Lager/JS/main.js
@@ -1,3 +1,40 @@
 //Beer Lager
 //Author: Michael J. Kelsey
 //For: Full Sail - ASD 1204
+
+$(document).ready(function(){
+
+	//Global Declarations - Assignments mad below
+	var hdrMainVar = null;  
+	var contentMainVar = null;  
+	var ftrMainVar = null;  
+	var contentTransitionVar = null;  
+	var stateLabelVar = null;  
+	var whatLabelVar = null;  
+	var stateVar = null;  
+	var whatVar = null;  
+	var form1var = null;  
+	var confirmationVar = null;  
+	var contentDialogVar = null;  
+	var hdrConfirmationVar = null;  
+	var contentConfirmationVar = null;  
+	var ftrConfirmationVar = null;  
+	var inputMapVar = null;  
+	
+	// Constants  
+	var MISSING = "missing";  
+	var EMPTY = "";  
+	var NO_STATE = "ZZ";
+	
+	//Assigned Global Variables
+	
+	hdrMainVar = $('#hdrMain');
+	contentMainVar = $('#contentMain');
+	ftrMainVar = $('#ftrMain');
+	contentTransitionVar = $('#contentTransition');
+	confirmationVar = $('#confirmation');
+	contentDialogVar = $('#contentDialog');
+	
+	
+	
+});
