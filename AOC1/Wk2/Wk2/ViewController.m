70ad38d93ba150764f7979b7064d639ba116f3ab
Michael Kelsey <kelseym@fullsail.edu>
2012-08-08 20:58:47 -0400

    changed background color to black

--- AOC1/Wk2/Wk2/ViewController.m
+++ AOC1/Wk2/Wk2/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad
 {
+    self.view.backgroundColor = [UIColor blackColor];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
