e66b1709643ad5b4db0d32d2d466bce921c9c568
Michael Kelsey <kelseym@fullsail.edu>
2012-08-16 21:25:31 -0400

    completed

--- AOC1/Wk3/Wk3/ViewController.m
+++ AOC1/Wk3/Wk3/ViewController.m
@@ -16,7 +16,28 @@
 
 - (void)viewDidLoad
 {
-
+    //Call the Append function with two NSStrings. Capture the result and display a UIAlertView with the appended string using displayAlertWithString.
+    NSString *appendStrings = [self append:@"I love " with:@"zombie movies!"];
+    [self displayAlertWithString:appendStrings];
+    
+    //Call the Add function passing in two integer values. Capture the return of this function into a variable.
+    int add = [self add:20 with:22];
+    
+    //Bundle the returned integer into an NSNumber and then convert it to a NSString and pass it to the DisplayAlertWithString function.
+    NSNumber *addedNumbers = [[NSNumber alloc]initWithInt:add];
+    
+    //Give it some text for the title. The message will read, "The number is 00". Replace the 00 with the integer passed into the function.
+    NSString *addedNumbersText = [NSString stringWithFormat:@"The meaning of life is "];
+    NSString *convertedNumberToString = [addedNumbers stringValue];
+    NSString *finalString = [self append:addedNumbersText with:convertedNumberToString];
+    [self displayAlertWithString:finalString];
+    
+    //Call the Compare function with two integer values. If Compare returns YES, display an UIAlertView both with the input values and the result using the DisplayAlertWithString function
+    int firstInt = 21;
+    int secondInt = 21;
+    BOOL compareInt = [self compare:firstInt with:secondInt];
+    NSString *compareAlert = [NSString stringWithFormat:@"The two numbers are %d and %d; Are they equal? %@", firstInt, secondInt, compareInt?@"YES":@"NO"];
+    [self displayAlertWithString:compareAlert];
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
