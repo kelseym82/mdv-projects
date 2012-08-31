bb5005575078bf79a89d0c47a5aba443412b34ce
Michael Kelsey <kelseym@fullsail.edu>
2012-08-30 21:06:57 -0400

    created zombie base class

--- AOC2/wk1/wk1/BaseZombie.h
+++ AOC2/wk1/wk1/BaseZombie.h
@@ -8,3 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
+//SOURCE FOR ZOMBIE TYPE IS http://wikigameguides.com/Dead_Island/wiki/Zombie_Types-15300
+typedef enum
+{
+    ZOMBIETYPE_FLOATER=0,
+    ZOMBIETYPE_INFECTED,
+    ZOMBIETYPE_BUTCHER
+}EZombieType;
+
+@interface BaseZombie : NSObject
+{
+    EZombieType zombieType;
+    NSString *zombieName;
+    int zombieSpeed;
+}
+
+@end
