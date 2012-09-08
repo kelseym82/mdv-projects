63a888a6e5bc0ee2069e0edab17b5c5e8a12e4ca
Michael Kelsey <kelseym@fullsail.edu>
2012-09-07 21:23:03 -0400

    The add and equal functions work properly.

--- AOC2/wk2/wk2/ViewController.m
+++ AOC2/wk2/wk2/ViewController.m
@@ -16,26 +16,28 @@
 
 -(IBAction)buttonNumberPressed:(id)sender
 {
-    NSLog(@"%.01f", currentNumber);
-    currentNumber = (float)[sender tag];
+    //The below pulls the current number from the tag on the button.  The math is needed to string the numbers together. For example, if I press the number 1 button, the tag = 1.  The current number default is 0, so you get 0*10 + 1 = 1, not if i press 1 again, the current number is now 1, so the math looks like this: 1*10 + 1 = 11.  Basically by multiplying the current number by 10 you move it over a dicimal place.
+    currentNumber = currentNumber*10 + (float)[sender tag];
     calculatorScreen.text = [NSString stringWithFormat:@"%.01f", currentNumber];
 }
 -(IBAction)buttonOperationPressed:(id)sender
 {
-    if (currentOperation == 0) result = currentNumber;
+    if (currentOperation == 0)
+    {
+        result = currentNumber;
+    }
     else
     {
         switch (currentOperation) {
             case 1:
                 result = result + currentNumber;
                 break;
-            case 2:
-                currentOperation = 0;
-                break;
         }
         
     }
+    //resets the current number
     currentNumber = 0;
+    //sends the result to the screen
     calculatorScreen.text = [NSString stringWithFormat:@"%.01f", result];
     if ([sender tag]==0) result = 0;
     currentOperation = [sender tag];
