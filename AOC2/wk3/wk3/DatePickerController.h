d64c0798a55a202b7858766a5e7067c43100d10a
Michael Kelsey <kelseym@fullsail.edu>
2012-09-13 18:14:55 -0400

    added second view, DatePickerController

---
+++ AOC2/wk3/wk3/DatePickerController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController+DatePickerController.h
+//  wk3
+//
+//  Created by Michael Kelsey on 9/13/12.
+//  Copyright (c) 2012 Michael Kelsey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController (DatePickerController)
+
+@end
---
+++ AOC2/wk3/wk3/DatePickerController.m
@@ -0,0 +1,13 @@
+//
+//  ViewController+DatePickerController.m
+//  wk3
+//
+//  Created by Michael Kelsey on 9/13/12.
+//  Copyright (c) 2012 Michael Kelsey. All rights reserved.
+//
+
+#import "ViewController+DatePickerController.h"
+
+@implementation ViewController (DatePickerController)
+
+@end
